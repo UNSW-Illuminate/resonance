b50f66a785ce4ca9412976017e523223d7cd310f
Jennifer Yu <jeyu1717@gmail.com>
2026-06-25 21:22:23 +1000

    add: cluster 2

--- entry/test_json.h
+++ entry/test_json.h
@@ -6,62 +6,62 @@ const char COORDINATES_JSON[] = R"json(
     {
       "x": 2987.73,
       "y": 3002.15,
-      "cluster_id": 0,
+      "cluster_id": 2,
       "cluster_index": 0
     },
     {
       "x": 3117.48,
       "y": 3010.07,
-      "cluster_id": 0,
+      "cluster_id": 2,
       "cluster_index": 9
     },
     {
       "x": 3269.14,
       "y": 3004.58,
       "cluster_id": 2,
-      "cluster_index": 0
+      "cluster_index": 18
     },
     {
       "x": 3102.9,
       "y": 2821.74,
-      "cluster_id": 0,
-      "cluster_index": 18
+      "cluster_id": 2,
+      "cluster_index": 90
     },
     {
       "x": 3309.02,
       "y": 2851.95,
-      "cluster_id": 0,
-      "cluster_index": 63
+      "cluster_id": 2,
+      "cluster_index": 27
     },
     {
       "x": 3159.45,
       "y": 2738.86,
-      "cluster_id": 0,
-      "cluster_index": 27
+      "cluster_id": 2,
+      "cluster_index": 81
     },
     {
       "x": 3295.08,
       "y": 2597.21,
-      "cluster_id": 0,
-      "cluster_index": 45
+      "cluster_id": 2,
+      "cluster_index": 72
     },
     {
       "x": 3283.56,
       "y": 2457.3,
-      "cluster_id": 0,
-      "cluster_index": 36
+      "cluster_id": 2,
+      "cluster_index": 63
     },
     {
       "x": 3409.77,
       "y": 2677.46,
-      "cluster_id": 0,
-      "cluster_index": 54
+      "cluster_id": 2,
+      "cluster_index": 36
     },
     {
       "x": 3394.62,
       "y": 2476.68,
       "cluster_id": 2,
-      "cluster_index": 9
+      "cluster_index": 54
     },
     {
       "x": 3422.6,
